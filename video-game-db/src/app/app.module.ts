2ed83886f6b010157da6909a92053a703396129d
krishankulasinghe <>
2022-03-26 20:28:39 -0400

    added modules

--- video-game-db/src/app/app.module.ts
+++ video-game-db/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule} from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { GaugeModule } from "angular-gauge";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatIconModule } from "@angular/material/icon";
+import {  MatFormFieldModule} from "@angular/material/form-field";
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule, 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
